2e4b157677f98480d8d3d54e6c119ad922ce6e10
jeremybierly <jeremybierly@hotmail.com>
2024-10-03 18:34:19 -0500

    adds a copy of the homepage to the blog route

---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold mb-8">Latest Posts</h1>
+    <div v-for="post in posts" :key="post._path" class="mb-8">
+      <h2 class="text-2xl font-semibold mb-2">
+        <NuxtLink :to="post._path" class="text-blue-600 hover:underline">
+          {{ post.title }}
+        </NuxtLink>
+      </h2>
+      <p class="text-gray-600">{{ post.description }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const { data: posts } = await useAsyncData('posts', () => queryContent('/blog').sort({ date: -1 }).limit(5).find())
+</script>
